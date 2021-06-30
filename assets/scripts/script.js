225dbea24a8737f4ba2688dd7aeab051d4f2230c
Ritika-Agrawal0811 <ritikaagrawal339@gmail.com>
2021-06-30 10:57:44 +0530

    changes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,8 +3,10 @@
 function viewNumber(head){
     let number = document.getElementById(head);
 
-    console.log(number);
+   
+        number.style.display = "block";
+        number.style.visibility = "visible";
+    
 
-    number.style.display = "block";
-    number.style.visibility = "visible";
+    
 }
